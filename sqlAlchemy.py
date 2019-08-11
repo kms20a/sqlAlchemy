6545160419ee7fa4c07e53036d7d702f390b6f1d
kms20a <kms21@naver.com>
2019-08-12 01:56:09 +0900

    Add Function - register, login

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref
 
@@ -56,6 +57,26 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='mskim Blog')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+            
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
